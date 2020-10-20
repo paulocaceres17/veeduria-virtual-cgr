ad7b628f2f1d4ceddd4dfab8a53e0da280f9f448
Paulo Ravi Cáceres <paulocaceres17@gmail.com>
2020-10-20 00:20:30 -0500

    change build

---
+++ docs/precache-manifest.6c6edbfd566bc918189ecbd46098de74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4900d725ed37c0e74fd640ea659d09b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "accb6e0871a56471f7f5",
+    "url": "/static/css/2.51270f71.chunk.css"
+  },
+  {
+    "revision": "802bb39166fe40b86598",
+    "url": "/static/css/main.83633844.chunk.css"
+  },
+  {
+    "revision": "accb6e0871a56471f7f5",
+    "url": "/static/js/2.a7769ff7.chunk.js"
+  },
+  {
+    "revision": "f17e9a723cbff1ef05bbe8fb96a452bd",
+    "url": "/static/js/2.a7769ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802bb39166fe40b86598",
+    "url": "/static/js/main.53d06e77.chunk.js"
+  },
+  {
+    "revision": "faf40aff7606873f8625",
+    "url": "/static/js/runtime-main.a327394c.js"
+  }
+]);
